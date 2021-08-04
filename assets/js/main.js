38c4d9cd2ab58497500c036706baa2ddb626a3e0
wisestme01 <wyhzest@gmail.com>
2021-08-04 21:01:16 +0100

    show menu

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// MENU SHOW Y HIDDEN
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
+  // MENU SHOW
+  // Validate if constant exists
+  if(navToggle)
+  {
+    navToggle.addEventListener('click', () => {
+      navMenu.classList.add('show-menu');
+    })
+  }
