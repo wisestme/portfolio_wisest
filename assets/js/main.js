c934b6e3a91cea53214cff0fea759958871c5b65
wisestme01 <wyhzest@gmail.com>
2021-08-07 22:36:00 +0100

    remove nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,13 @@ if(navClose)
   navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu');
   navToggle.classList.remove('hide-icon');
-
   });
 
 }
+
+// Remove Menu
+const navLink = document.querySelectorAll('.nav__link');
+
+navLink.forEach(n => n.addEventListener('click', function (){
+  navMenu.classList.remove('show-menu');
+}))
