bffb3ebba6872a641b89d8df42eb9208d2c452b9
wisestme01 <wyhzest@gmail.com>
2021-08-17 20:26:09 +0100

    toggle active tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,22 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener('click', toggleSkills);
 })
+
+// Qualification Tabs
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active');
+    })
+    target.classList.add('qualification__active');
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification__active');
+    })
+    tab.classList.add('qualification__active');
+  })
+})
